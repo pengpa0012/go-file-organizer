06ca616c5da4dc952ac4036b08a7a2705cef3a31
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-13 08:19:42 +0800

    added loop on user input

--- main.go
+++ main.go
@@ -8,23 +8,33 @@ import (
 	"strings"
 )
 func main() {
-	fmt.Println("input command:")
+	for {
+		line := readLine()
+		// Remove white spaces and new line on user input
+		line = strings.ReplaceAll(line, " ", "")
+		line = strings.ReplaceAll(line, "\n", "")
 
+		if line[:len(line)-1] == "close" {
+			fmt.Println("Exiting the program.")
+			break
+		}
+
+		if line[:len(line)-1] == "show" {
+			showDirectories()
+		} else {
+			fmt.Println("Invalid command. Try again.")
+		}
+	}
+}
+
+func readLine() string {
 	reader := bufio.NewReader(os.Stdin)
 	line, err := reader.ReadString('\n')
 
 	if err != nil {
 			log.Fatal(err)
 	}
-
-	// removed white spaces and new line on user input
-	line = strings.ReplaceAll(line, " ", "")
-	line = strings.ReplaceAll(line, "\n", "")
-
-	if line[:len(line)-1] == "show" {
-		showDirectories()
-	}
-
+	return line
 }
 
 func showDirectories() {
@@ -37,3 +47,4 @@ func showDirectories() {
 		fmt.Println(f.Name())
 	}
 }
+
