c9d8eed0f591d97ccac4155016c9051d430e848a
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-13 08:13:29 +0800

    added show current directory

--- main.go
+++ main.go
@@ -1,6 +1,39 @@
 package main
 
-import "fmt" 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("input command:")
+
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadString('\n')
+
+	if err != nil {
+			log.Fatal(err)
+	}
+
+	// removed white spaces and new line on user input
+	line = strings.ReplaceAll(line, " ", "")
+	line = strings.ReplaceAll(line, "\n", "")
+
+	if line[:len(line)-1] == "show" {
+		showDirectories()
+	}
+
+}
+
+func showDirectories() {
+	files, err := os.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 }
