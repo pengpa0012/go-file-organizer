3fb9eb2c86d5ada771ee96f20bebad1c4afe6f44
Pengpa0012 <peng.ilikha@gmail.com>
2024-03-14 08:55:39 +0800

    added select directory, error handling

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ func main() {
 	for {
 		line := readLine()
 		// Remove white spaces and new line on user input
-		line = strings.ReplaceAll(line, " ", "")
 		line = strings.ReplaceAll(line, "\n", "")
 
 		if line[:len(line)-1] == "close" {
@@ -20,11 +19,14 @@ func main() {
 			break
 		} else if line[:len(line)-1] == "back" {
 			changeDirectory()
+		} else if strings.Fields(line)[0] == "select" && strings.Fields(line)[1] != "" {
+			moveDirectory(strings.Fields(line)[1])
 		} else if line[:len(line)-1] == "show" {
 			showDirectories()
 		} else {
 			fmt.Println("Invalid command. Try again.")
 		}
+		
 	}
 }
 
@@ -57,4 +59,26 @@ func changeDirectory() {
 	}
 	newPath := filepath.Dir(currentDir)
 	os.Chdir(newPath)
+}
+
+func moveDirectory(path string) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		fmt.Println("Invalid directory:", err)
+		return
+	}
+
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current directory:", err)
+		return
+	}
+
+	if fileInfo.IsDir() {
+		newPath := filepath.Join(currentDir, path)
+		os.Chdir(newPath)
+	} else {
+		fmt.Println("Invalid directory:", err)
+		return
+	}
 }
